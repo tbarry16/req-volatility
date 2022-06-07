4828cb9d8f5496d428ebc0855878dd8821f60863
Michael Watson <mdwatson988@gmail.com>
2022-06-07 18:16:05 -0400

    set up OAuth login functionality

--- client/app.js
+++ client/app.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Login from './pages/login.jsx';
 
+const UserContext = createContext();
 
 const App = () => {
+  const [user, setUser] = useState(undefined);
+  const userObj = {
+    user: user,
+    setUser: setUser
+  }
 
   return(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/Login' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={userObj}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/Login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 };
 
 export default App;
+export { UserContext };
